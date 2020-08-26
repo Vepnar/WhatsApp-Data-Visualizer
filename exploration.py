94647efc3042a6849c2fe6407b3ce164a4715ed6
Vepnar <agdehaan@gmail.com>
2020-08-26 18:12:03 +0200

    Updated emoticon filter

--- exploration.py
+++ exploration.py
@@ -17,6 +17,7 @@ TODO: Add argument and return description
 """
 
 import re
+import time
 from datetime import datetime
 
 # Note: My system language is dutch. update these strings to fit your language.
@@ -24,14 +25,27 @@ EXTERNAL_MEDIA = '<Media weggelaten>'
 MESSAGE_DELETED = 'U hebt dit bericht verwijderd'
 
 # Regular exptression to discard all emojis
-# Source: https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python
-EMOJI_PATTERN = re.compile(
-    pattern="["
-    u"\U0001F600-\U0001F64F"  # emoticons
-    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-    u"\U0001F680-\U0001F6FF"  # transport & map symbols
-    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "]+", flags=re.UNICODE)
+# Source: https://gist.github.com/slowkow/7a7f61f495e3dbb7e3d767f97bd7304b#gistcomment-3360005
+EMOJI_PATTERN = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002500-\U00002BEF"  # chinese char
+                           u"\U00002702-\U000027B0"
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           u"\U0001f926-\U0001f937"
+                           u"\U00010000-\U0010ffff"
+                           u"\u2640-\u2642"
+                           u"\u2600-\u2B55"
+                           u"\u200d"
+                           u"\u23cf"
+                           u"\u23e9"
+                           u"\u231a"
+                           u"\ufe0f"  # dingbats
+                           u"\u3030"
+                           "]+", flags=re.UNICODE)
 
 
 def emoji_filter(line):
@@ -39,7 +53,7 @@ def emoji_filter(line):
     return EMOJI_PATTERN.sub(r'', line)
 
 
-def parse_lines(line: str, remove_media=True, remove_deleted=True, remove_emojis=True) -> tuple:
+def parse_lines(line: str, remove_media=True, remove_deleted=True, remove_emojis=True, minimum_length=3) -> tuple:
     """Data cleaning
 
     This part of the code does the essential cleaning and converting of usefull variables.
@@ -62,12 +76,16 @@ def parse_lines(line: str, remove_media=True, remove_deleted=True, remove_emojis
             return
 
         # Remove deleted messages when this is enabled.
-        if remove_deleted in MESSAGE_DELETED in message:
+        if remove_deleted and MESSAGE_DELETED in message:
             return
 
         # Remove emoticons when this is enabled.
         if remove_emojis:
             message = emoji_filter(message)
+
+        # Remove messages below the given thresholdv
+        if minimum_length > len(message):
+            return
 
         return date, sender, message
 
