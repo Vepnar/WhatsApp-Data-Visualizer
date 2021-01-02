2400b758948e42551d3c87f2dd2465569f431284
Vepnar <agdehaan@gmail.com>
2021-01-02 16:10:57 +0100

    Created a stub

---
+++ markov-chat-chatbot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Create a chatbot based on the dataset with continuous-time markov chain
+
+Author: Arjan de Haan (Vepnar)
+Created: 30 December 2020
+"""
+import numpy as np
+
+# Markov chain stored as adjacency list.
+LEXICON = {}
+
+# Length of the 
+WORD_LENGTH = 2
