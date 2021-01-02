793cb1ab3b952265e6f511e3e28d6d45efdee4f8
Vepnar <agdehaan@gmail.com>
2021-01-02 16:43:45 +0100

    Detect line endings

--- markov-chat-chatbot.py
+++ markov-chat-chatbot.py
@@ -4,8 +4,9 @@
 """Create a chatbot based on the dataset with continuous-time markov chain
 
 Author: Arjan de Haan (Vepnar)
-Created: 30 December 2020
+Created: 1 January 2021
 """
+import re
 import numpy as np
 
 # Markov chain stored as adjacency list.
@@ -13,3 +14,16 @@ LEXICON = {}
 
 # Length of the 
 WORD_LENGTH = 2
+
+LINE_END_REGEX = re.compile(r'(\.|\!|\?|\n)')
+
+def is_ending(line : str) -> bool:
+    """Detect if the current line is ending.
+
+    Args:
+        line (str): Line that should be checked.
+
+    Returns:
+        bool: True when it is ending and false when it isn't ending.
+    """
+    return len(LINE_END_REGEX.findall(line)) > 0
